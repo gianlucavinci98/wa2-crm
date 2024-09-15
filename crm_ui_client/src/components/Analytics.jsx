f6e122ade10a468c6b3c478a80718c7b0ff7fb4d
AlessandroBelasri <belalessandro1@gmail.com>
2024-09-15 18:26:05 +0200

    minor fix

--- crm_ui_client/src/components/Analytics.jsx
+++ crm_ui_client/src/components/Analytics.jsx
@@ -51,8 +51,10 @@ function AnalyticsCharts() {
         <LineChart width={600} height={300} data={data} margin={{top: 5, right: 20, bottom: 5, left: 0}}>
             <Line type="monotone" dataKey="timeSpent" stroke="#8884d8"/>
             <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
+            {/*
             <XAxis dataKey="name"/>
             <YAxis/>
+            */}
             <Tooltip/>
         </LineChart>
     )
