58b580054bac2f6f48e807d5203798e03e50e6ab
AlessandroBelasri <belalessandro1@gmail.com>
2024-09-15 17:47:26 +0200

    minor fix

--- crm_ui_client/src/components/Analytics.jsx
+++ crm_ui_client/src/components/Analytics.jsx
@@ -14,7 +14,7 @@ function AnalyticsCharts() {
     useEffect(() => {
         if (!load) {
             AnalyticsAPI.GetElapsedStatusTime().then((result) => {
-                for (let i = 0; i < result.timeStatistic.length; i++) { //Number of jobOffers
+                    for (let i = 0; i < result.timeStatistic.length; i++) { //Number of jobOffers
                         for (let j = 0; j < 6; j++) { //Number of possible states
                             if (result.timeStatistic[i].jobOfferHistory[j] != null) { //The data related to that status exists
                                 numberOfElements[j]++;
@@ -28,10 +28,19 @@ function AnalyticsCharts() {
                     for (let i = 0; i < 6; i++) {
                         if (numberOfElements[i] !== 0)
                             averageTimes[i] = averageTimes[i] / numberOfElements[i]
+                        else
+                            averageTimes[i] = 0
                     }
 
-                setData()
-                setLoad(true)
+                    setData( [
+                        {name: "Created", timeSpent: averageTimes[0]},
+                        {name: "Selection Phase", timeSpent: averageTimes[1]},
+                        {name: "Candidate Proposal", timeSpent: averageTimes[2]},
+                        {name: "Consolidated", timeSpent: averageTimes[3]},
+                        {name: "Done", timeSpent: averageTimes[4]},
+                        {name: "Aborted", timeSpent: averageTimes[5]}
+                    ])
+                    setLoad(true)
                 }
             ).catch((err) => console.log(err))
         }
@@ -39,8 +48,8 @@ function AnalyticsCharts() {
 
 
     return (
-        <LineChart width={600} height={300} data={averageTimes} margin={{top: 5, right: 20, bottom: 5, left: 0}}>
-            <Line type="monotone" dataKey="uv" stroke="#8884d8"/>
+        <LineChart width={600} height={300} data={data} margin={{top: 5, right: 20, bottom: 5, left: 0}}>
+            <Line type="monotone" dataKey="timeSpent" stroke="#8884d8"/>
             <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
             <XAxis dataKey="name"/>
             <YAxis/>
