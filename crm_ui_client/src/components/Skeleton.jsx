ba2a35198e6fcc229767dd66856f5124ca996f77
AlessandroBelasri <belalessandro1@gmail.com>
2024-09-18 15:41:43 +0200

    minor fix

--- crm_ui_client/src/components/Skeleton.jsx
+++ crm_ui_client/src/components/Skeleton.jsx
@@ -44,7 +44,7 @@ function TopBar({switchFilter, openFilter, addNew, setAddNew, filterPresent, cur
                         }
                         {
                             (
-                                location.pathname.split("/").pop() !== 'ui' && !location.pathname.includes("Messages") && !location.pathname.includes("Professionals") && !location.pathname.includes("Customers") && !location.pathname.includes("JobOffers") && !location.pathname.includes("Analytics"))
+                                location.pathname.split("/").pop() !== 'ui' && !location.pathname.includes("Messages") && !location.pathname.includes("Professionals") && !location.pathname.includes("Customers") && !location.pathname.includes("JobOffers") && !location.pathname.includes("Analytics") && !location.pathname.includes("Documents"))
                                 ?
                                 !addNew ?
                                     (!(currentUser?.roles.filter(it => it.includes('recruiter')).length > 0 && location.pathname.includes("Customers")) &&
