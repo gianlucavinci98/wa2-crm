4f983507221660a9300ff6a7fe9c3bbef0346257
morac <moracci99@gmail.com>
2024-09-18 12:43:54 +0200

    minor fix

--- crm_ui_client/src/components/JobofferForm.jsx
+++ crm_ui_client/src/components/JobofferForm.jsx
@@ -245,70 +245,83 @@ function JobOfferForm({currentUser}) {
     return (
         jobOfferId === undefined
             ?
-            <form className={"flex flex-col flex-1 justify-around items-center"} onSubmit={onSubmitHandler}>
-                <h1 className={"text-2xl font-semibold"}>Create new job offer</h1>
-                {serverResponse && (<div className={"w-full bg-blue-100 text-center p-4"}>{serverResponse}</div>)}
-                <div className={"flex flex-col gap-6 justify-around"}>
-                    <div className={"col-field"}>
-                        <label className={""}>Customer:</label>
-                        <input className={"flex-1"} type={"text"}
-                               value={"" + customer.contact.name + " " + customer.contact.surname}
-                               disabled={true}/>
+            <div className={"h-full w-full flex flex-col justify-between p-4"}>
+                <div className="w-full flex flex-row justify-center items-start">
+                    <div className="flex flex-row justify-start">
+                        <button onClick={() => navigate("/ui/Clients")}>
+                            <FaArrowLeft size={20}/>
+                        </button>
                     </div>
-                    <div className={"col-field"}>
-                        <div className={"h-full flex flex-col justify-between"}>
-                            <label className={errors.description ? "text-red-500" : ""}>Description:</label>
-                            <div>{jobOffer.description.length}/1000</div>
-                        </div>
-                        <textarea
-                            name={"description"}
-                            className={"h-52"}
-                            value={jobOffer.description}
-                            maxLength={1000}
-                            cols={50}
-                            required={true}
-                            placeholder="Insert description here..."
-                            onChange={onChangeHandler}/>
-
+                    <div className="flex flex-1 flex-row justify-center">
+                        <h1 className="font-bold text-2xl">New Job Offer</h1>
                     </div>
-
-                    <div className={"col-field"}>
-                        <label className={errors.requiredSkills ? "text-red-500" : ""}>Required skills:</label>
-                        <div className={"flex flex-col gap-2 flex-1"}>
-                            <div className={"flex-1 flex gap-2 items-center"}>
-                                <input type={"text"} name={"requiredSkills"} value={currentSkill}
-                                       className={"p-2 flex-1"}
-                                       placeholder={"Insert a required skill here..."}
-                                       onChange={(e) => setCurrentSkill(e.target.value)}/>
-                                <Icon className={"w-4 h-4 fill-blue-500"} name={"plus"} onClick={onAdd}/>
-                            </div>
-
-                            {
-                                jobOffer.requiredSkills.map((item, index) =>
-                                    <div className={"flex items-center gap-2"}
-                                         key={index}>
-                                        <p>{item}</p>
-                                        <Icon className={"w-4 h-4 fill-red-500"} name={"cross"}
-                                              onClick={() => onRemove(index)}/>
-                                    </div>
-                                )
-                            }
-                        </div>
-                    </div>
-
-                    <div className={"col-field"}>
-                        <label className={errors.duration ? "text-red-500" : ""}>Duration of contract in
-                            days:</label>
-                        <input className={"flex-1"} type={"number"} name={"duration"} min={1} max={1825}
-                               value={jobOffer.duration.toString()}
-                               required={true} onChange={onChangeHandler}/>
-                    </div>
+                    <div></div>
                 </div>
-                <button className={"page-button"} type="submit" onSubmit={onSubmitHandler}
-                        disabled={isSubmitting}>
-                    {isSubmitting ? "Submitting..." : "Create"}
-                </button>
-            </form>
+                <form className={"flex flex-col flex-1 justify-around items-center"} onSubmit={onSubmitHandler}>
+                    <h1 className={"text-2xl font-semibold"}>Create new job offer</h1>
+                    {serverResponse && (<div className={"w-full bg-blue-100 text-center p-4"}>{serverResponse}</div>)}
+                    <div className={"flex flex-col gap-6 justify-around"}>
+                        <div className={"col-field"}>
+                            <label className={""}>Customer:</label>
+                            <input className={"flex-1"} type={"text"}
+                                   value={"" + customer.contact.name + " " + customer.contact.surname}
+                                   disabled={true}/>
+                        </div>
+                        <div className={"col-field"}>
+                            <div className={"h-full flex flex-col justify-between"}>
+                                <label className={errors.description ? "text-red-500" : ""}>Description:</label>
+                                <div>{jobOffer.description.length}/1000</div>
+                            </div>
+                            <textarea
+                                name={"description"}
+                                className={"h-52"}
+                                value={jobOffer.description}
+                                maxLength={1000}
+                                cols={50}
+                                required={true}
+                                placeholder="Insert description here..."
+                                onChange={onChangeHandler}/>
+
+                        </div>
+
+                        <div className={"col-field"}>
+                            <label className={errors.requiredSkills ? "text-red-500" : ""}>Required skills:</label>
+                            <div className={"flex flex-col gap-2 flex-1"}>
+                                <div className={"flex-1 flex gap-2 items-center"}>
+                                    <input type={"text"} name={"requiredSkills"} value={currentSkill}
+                                           className={"p-2 flex-1"}
+                                           placeholder={"Insert a required skill here..."}
+                                           onChange={(e) => setCurrentSkill(e.target.value)}/>
+                                    <Icon className={"w-4 h-4 fill-blue-500"} name={"plus"} onClick={onAdd}/>
+                                </div>
+
+                                {
+                                    jobOffer.requiredSkills.map((item, index) =>
+                                        <div className={"flex items-center gap-2"}
+                                             key={index}>
+                                            <p>{item}</p>
+                                            <Icon className={"w-4 h-4 fill-red-500"} name={"cross"}
+                                                  onClick={() => onRemove(index)}/>
+                                        </div>
+                                    )
+                                }
+                            </div>
+                        </div>
+
+                        <div className={"col-field"}>
+                            <label className={errors.duration ? "text-red-500" : ""}>Duration of contract in
+                                days:</label>
+                            <input className={"flex-1"} type={"number"} name={"duration"} min={1} max={1825}
+                                   value={jobOffer.duration.toString()}
+                                   required={true} onChange={onChangeHandler}/>
+                        </div>
+                    </div>
+                    <button className={"page-button"} type="submit" onSubmit={onSubmitHandler}
+                            disabled={isSubmitting}>
+                        {isSubmitting ? "Submitting..." : "Create"}
+                    </button>
+                </form>
+            </div>
             :
             !load
                 ?
